44350aa30ffc45cf57c4ebafd12d050eadd77e92
Almog <almogbh.l@gmail.com>
2019-06-15 13:53:48 +0300

    Updates

---
+++ precache-manifest.8f266f41dea6f4e69233700c76f1ad5e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8305d2d2f7592b9977f8c07e02a98252",
+    "url": "/musicStore/index.html"
+  },
+  {
+    "revision": "1b2c441f5a0e778cc74a",
+    "url": "/musicStore/static/css/2.a19b2401.chunk.css"
+  },
+  {
+    "revision": "1b2c441f5a0e778cc74a",
+    "url": "/musicStore/static/js/2.d4891491.chunk.js"
+  },
+  {
+    "revision": "d9573d4c17b521d5fc51",
+    "url": "/musicStore/static/js/main.780805c3.chunk.js"
+  },
+  {
+    "revision": "147204bb56a92e7eee02",
+    "url": "/musicStore/static/js/runtime~main.18b3f6d8.js"
+  }
+]);
